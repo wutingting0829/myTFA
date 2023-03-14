e31881d017cee684ccd4a9397fd5bebcb3377d11
wutingting0829 <tingiscute520@gmail.com>
2023-03-14 16:04:19 +0800

    new user profile model to admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from blog.models import user_directory_path
 
 # Create your models here.
+
+# customer path to store my image data or store the user's avatar images
+def user_directory_path(instance, filename):
+    return 'user/avatars/{0}/{1}'.format(instance.user.id, filename)
+
+
+
+# one to one connection between this table and the users table
+# on_delete means it should delete a user in the user table and also should delete the user in this profile table
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # the user profile img
+    avatar = models.ImageField(upload_to=user_directory_path, default='user/avatar.jpg')
+
+    bio = models.TextField(max_length=500, blank=True)
