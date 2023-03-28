b06aaf866a395836fc157a055e2cef6dc4f30646
wutingting0829 <tingiscute520@gmail.com>
2023-03-28 21:46:10 +0800

    front-end modification

--- fuzzy/forms.py
+++ fuzzy/forms.py
@@ -3,11 +3,11 @@ from django import forms
 
 class FuzzyExtractorForm(forms.Form):
     old_password = forms.CharField(
-        label='field1', widget=forms.PasswordInput(
+        label='field 1', widget=forms.PasswordInput(
             attrs={'class': 'form-control mb-3 col-sm-12', 'placeholder': 'Old Password', 'id': 'form-oldpass'}))
 
 
 class FuzzyExtractorReproduceForm(forms.Form):
     helper = forms.CharField(
-        label='field1', widget=forms.PasswordInput(
+        label='field 2', widget=forms.PasswordInput(
             attrs={'class': 'form-control mb-3 col-sm-12', 'placeholder': 'Old Password', 'id': 'form-oldpass1111'}))
